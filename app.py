b59e31c284fc9470d70415a620fd395a4b6f2849
boleesystem <boleesystem@gmail.com>
2022-12-09 06:27:30 +0900

    debugging success - mypage edit

--- app.py
+++ app.py
@@ -2,35 +2,35 @@ from flask import Flask, render_template, session, url_for, request, redirect
 import pymysql
 import hashlib
 
-'''
-# 로깅시스템 - 시작
-import logging
-import datetime
-from pytz import timezone
-# 로깅시스템 - 끝 (인터프리터 pytz)
-'''
+
+# # 로깅시스템 - 시작
+# import logging
+# import datetime
+# from pytz import timezone
+# # 로깅시스템 - 끝 (인터프리터 pytz)
+
 app = Flask(__name__)
 app.secret_key = '1234'
 
-'''
-# 로깅시스템 - 시작
-logging.basicConfig(filename = "logs/test.log", level = logging.DEBUG)
 
-def log(request, message):
-    log_date = get_log_date()
-    log_message = "{0}/{1}/{2}".format(log_date, str(request), message)
-    logging.error(log_message)
-def error_log(request, error_code, error_message):
-    log_date = get_log_date()
-    log_message = "{0}/{1}/{2}/{3}".format(log_date, str(request), error_code, error_message)
-    logging.error(log_message)
+# # 로깅시스템 - 시작
+# logging.basicConfig(filename = "logs/test.log", level = logging.DEBUG)
+#
+# def log(request, message):
+#     log_date = get_log_date()
+#     log_message = "{0}/{1}/{2}".format(log_date, str(request), message)
+#     logging.error(log_message)
+# def error_log(request, error_code, error_message):
+#     log_date = get_log_date()
+#     log_message = "{0}/{1}/{2}/{3}".format(log_date, str(request), error_code, error_message)
+#     logging.error(log_message)
+#
+# def get_log_date():
+#     dt = datetime.datetime.now(timezone("Asia/Seoul"))
+#     log_date = dt.strftime("%Y%m%d_%H:%M:%S")
+#     return log_date
+# # 로깅시스템 - 끝
 
-def get_log_date():
-    dt = datetime.datetime.now(timezone("Asia/Seoul"))
-    log_date = dt.strftime("%Y%m%d_%H:%M:%S")
-    return log_date
-# 로깅시스템 - 끝
-'''
 
 def connectsql():
     conn = pymysql.connect(host='localhost', user = 'root', passwd = '0000', db = 'userlist', charset='utf8')
@@ -64,8 +64,6 @@ def post():
     conn.close()
 
     return render_template('post.html', postlist = post_list, logininfo=username)
-
-# mydata
 
 
 @app.route('/post/content/<id>')
@@ -209,6 +207,8 @@ def write():
         else:
             return render_template ('Error.html')
 
+
+
 @app.route('/logout')
 # username 세션 해제
 def logout():
@@ -243,9 +243,6 @@ def login():
             session['username'] = request.form['username']
             session['password'] = request.form['password']
 
-            #session['username'] = request.form['name']
-            #session['userpwd'] = request.form['pwd']
-
             return render_template('index.html', logininfo = logininfo)
         else:
             return render_template('loginError.html')
@@ -260,8 +257,6 @@ def regist():
         username = request.form['username']
         password = request.form['password']
         userintro = request.form['intro']
-        #userid = request.form['id']
-        #userpw = request.form['pw']
 
         conn = connectsql()
         cursor = conn.cursor()
@@ -269,7 +264,7 @@ def regist():
         value = username
         cursor.execute(query, value)
         data = (cursor.fetchall())
-        #import pdb; pdb.set_trace()
+
         if data:
             conn.rollback() # 이건 안 써도 될 듯
             return render_template('registError.html')
@@ -286,106 +281,32 @@ def regist():
     else:
         return render_template('regist.html')
 
-# # 마이페이지로 가는 버튼 부분 테스트 - 시작
-# @app.route('/mypage/<myid>')
-# # GET -> 유지되고있는 username 세션과 현재 접속되어진 id와 일치시 edit페이지 연결
-# # POST -> 접속되어진 id와 일치하는 title, content를 찾아 UPDATE
-# def mydata(myid):
-#     if 'username' in session:
-#         username = session['username']
-#
-#         conn = connectsql()
-#         cursor = conn.cursor()
-#         query = f"SELECT user_name, user_intro FROM user WHERE user_name = '{myid}'" # where user_id로 잘못써서 mysql에서 데이터 안나옴
-#         print(query)
-#         cursor.execute(query)
-#         data = [myinfo[0] for myinfo in cursor.fetchall()]
-#         print(data)
-#         cursor.close()
-#         conn.close()
-#         return render_template('mypage.html', mydatalist=mydata, logininfo=username)
-#
-#         # if username in data:
-#         #     conn = connectsql()
-#         #     cursor = conn.cursor(pymysql.cursors.DictCursor)
-#         #     query = f"SELECT user_name, user_intro FROM user WHERE user_name = '{myid}'" # where user_id로 잘못써서 mysql에서 데이터 안나옴
-#         #     print(query)
-#         #     cursor.execute(query)
-#         #     mydata = cursor.fetchall()
-#         #     print(mydata)
-#         #     cursor.close()
-#         #     conn.close()
-#         #     return render_template('mypage.html', mydatalist=mydata, logininfo=username)
-#        # else:
-#         #    return render_template('editError.html') # 본인게시글이 아닙니다!
-#     else:
-#         return render_template ('Error.html') # 로그인하세요!
-# # 마이페이지로 가는 버튼 부분 테스트 - 끝
-
-
-마이페이지 - 테스트
-
-    array = cursor.fetchall()
-        for i in range(len(array)):
-            print(array[i])
-            print(len(array))
-
-           print(array[i]['user_name'])
-           print(array[i]['user_intro'])
-
-        user_name, user_intro = cursor.fetchone()
-        print(user_name)
-        print(user_intro)
-        mycontent = [myinfo[0] for myinfo in cursor.fetchall()] fetchall 왜쓰냐.. 유저들, 전체검색할때임 한명일땐 fetchone
-        print(mycontent)
 
 @app.route('/mypage/<myid>')
+# 마이페이지 - 페이지의 게시글 클릭시 id와 content 비교 후 게시글 내용 출력
 def mycontent(myid):
     if 'username' in session:
         username = session['username']
+
         conn = connectsql()
-        # cursor = conn.cursor()
         cursor = conn.cursor(pymysql.cursors.DictCursor)
-        query = f"SELECT user_name, user_intro FROM user WHERE user_name = '{myid}'"  # where user_id로 잘못써서 mysql에서 데이터 안나옴
-        print(query)
+        # query = "SELECT user_name, user_intro FROM user WHERE user_id"
+        query = f"SELECT user_name, user_intro FROM user WHERE user_name = '{myid}'"
 
         cursor.execute(query)
-        result = cursor.fetchone()
-        # print(result)
-        print(result['user_name'])
-        print(result['user_intro'])
-
+        mycontent = cursor.fetchall()
+        conn.commit()
         cursor.close()
         conn.close()
-
-        return render_template('mypage.html', mydata = 'result', username = username)
-
-
-# # 마이페이지 - 잘 작동하는 코드 (post.html에서 링크 눌렀을 때 mypage.html과 연결안됨)
-# @app.route('/mypage/<myid>')
-# # 페이지의 게시글 클릭시 id와 content 비교 후 게시글 내용 출력
-# def mycontent(myid):
-#     if 'username' in session:
-#         username = session['username']
-#
-#         conn = connectsql()
-#         cursor = conn.cursor(pymysql.cursors.DictCursor)
-#         # query = "SELECT user_name, user_intro FROM user WHERE user_id"
-#         query = f"SELECT user_name, user_intro FROM user WHERE user_name = '{myid}'"
-#         cursor.execute(query)
-#         mycontent = cursor.fetchall()
-#         conn.commit()
-#         cursor.close()
-#         conn.close()
-#         return render_template('mypage.html', mydata = mycontent, username = username)
-#     else:
-#         return render_template ('Error.html')
+        return render_template('mypage.html', mydata = mycontent, username = username)
+    else:
+        return render_template ('Error.html')
 
 
-@app.route('/mypage/edit/<myid>', methods=['GET', 'POST'])
+@app.route('/mypage/edit/<user_name>', methods=['GET', 'POST'])
 # GET -> 유지되고있는 username 세션과 현재 접속되어진 id와 일치시 edit페이지 연결
 # POST -> 접속되어진 id와 일치하는 title, content를 찾아 UPDATE
-def mycontentedit(myid):
+def mycontentedit(user_name):
     if request.method == 'POST':
         if 'username' in session:
             username = session['username']
@@ -395,20 +316,27 @@ def mycontentedit(myid):
 
             conn = connectsql()
             cursor = conn.cursor()
-            query = f"UPDATE user SET user_name = %s, user_intro = %s WHERE user_name = '{myid}'"
-            #value = (myusername, myuserintro)
-            cursor.execute(query)
+            query = "UPDATE user SET user_name = %s, user_intro = %s WHERE user_name = %s"
+
+            value = (myusername, myuserintro, user_name)
+            cursor.execute(query, value)
+            # query = f"SELECT user_name, user_intro FROM user WHERE user_name = '{user_name}'"
+            # cursor.execute(query)
+
+            # mycontentedit = cursor.fetchall()
+            # print(mycontentedit)
+
             conn.commit()
             cursor.close()
             conn.close()
-
-            return render_template('editSuccess.html')
+            return redirect(f'/mypage/{user_name}')
+            # return render_template('mypageEdit.html', mydataedit=mycontentedit)
     else:
         if 'username' in session:
             username = session['username']
             conn = connectsql()
             cursor = conn.cursor()
-            query = f"SELECT user_name, user_intro FROM user WHERE user_name = '{myid}'" # where user_id로 잘못써서 mysql에서 데이터 안나옴
+            query = f"SELECT user_name, user_intro FROM user WHERE user_name = '{user_name}'" # where user_id로 잘못써서 mysql에서 데이터 안나옴
             cursor.execute(query)
             data = [myinfo[0] for myinfo in cursor.fetchall()]
             cursor.close()
@@ -417,9 +345,10 @@ def mycontentedit(myid):
             if username in data:
                 conn = connectsql()
                 cursor = conn.cursor(pymysql.cursors.DictCursor)
-                query = f"SELECT user_name, user_intro FROM user WHERE user_name = '{myid}'" # where user_id로 잘못써서 mysql에서 데이터 안나옴
+                query = f"SELECT user_name, user_intro FROM user WHERE user_name = '{user_name}'" # where user_id로 잘못써서 mysql에서 데이터 안나옴
                 cursor.execute(query)
                 mycontentedit = cursor.fetchall()
+
                 cursor.close()
                 conn.close()
                 return render_template('mypageEdit.html', mydataedit=mycontentedit, logininfo=username)
@@ -430,3 +359,4 @@ def mycontentedit(myid):
 
 if __name__ == '__main__':
     app.run(debug=True)
+
